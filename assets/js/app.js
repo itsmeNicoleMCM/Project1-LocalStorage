195e28633f0fb6d10e49e3b41d95a5b89b89a516
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 20:06:10 +0800

    First step with the project

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// Variables
+
+
+
+  // Event Listener 
+  eventListeners();
+  
+  function eventListeners() {
+       //  Form Submission 
+       document.querySelector('#form').addEventListener('submit', newTweet);
+}
+
+
+  // Functions
+  
+  function newTweet(e) {
+       e.preventDefault();
+
+       console.log('Form Submitted');
+}
