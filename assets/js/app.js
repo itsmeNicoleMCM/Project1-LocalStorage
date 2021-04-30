c624288dd39cb40ee2d5c63606ea5f2c2fbd99c6
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 20:10:24 +0800

    Adding Elements into the Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,6 +35,9 @@ function newTweet(e) {
      
      // Add to the list
      tweetList.appendChild(li);
+
+     // add to Local Storage
+     addTweetLocalStorage(tweet);
 }      
 
 
@@ -43,4 +46,28 @@ function removeTweet(e) {
      if(e.target.classList.contains('remove-tweet')) {
           e.target.parentElement.remove();
      }
+}       
+
+
+// Add the tweets into local storage
+function addTweetLocalStorage(tweet) {
+}.         let tweets = getTweetsFromStorage();
+     
+     // Add the tweet into the array 
+     tweets.push(tweet);
+     
+     // Convert tweet array into string
+     localStorage.setItem('tweets',  JSON.stringify(tweets )  );
+}
+
+function getTweetsFromStorage() {
+     let tweets;
+     const tweetsLS = localStorage.getItem('tweets');
+     // Get the values. if null is returned then we create an empty array
+     if(tweetsLS === null) {
+          tweets = [];
+     } else {
+          tweets = JSON.parse( tweetsLS );
+     }
+     return tweets;
 }
